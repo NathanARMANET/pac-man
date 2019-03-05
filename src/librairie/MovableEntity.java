316a27b381310f3070e1487097a98de53df79726
ARMANET Nathan <nathan.armanet@gmail.com>
2019-03-05 15:55:09 +0100

    creation vitesse pour MovableEntity

--- src/librairie/MovableEntity.java
+++ src/librairie/MovableEntity.java
@@ -2,29 +2,47 @@ package librairie;
 
 public class MovableEntity extends Entity {
 
+    private double _speed;
     private Direction _d;
 
-    public MovableEntity(double x, double y, CollisionBox hitBox) {
+    public MovableEntity(double x, double y, CollisionBox hitBox, double speed) {
         super(x, y, hitBox);
         _d = Direction.immobile;
+        _speed = speed;
     }
 
-    public MovableEntity(double x, double y, double height, double width) {
-        this(x, y, new CollisionBox(height, width));
+    public MovableEntity(double x, double y, double height, double width, double speed) {
+        this(x, y, new CollisionBox(height, width), speed);
+    }
+
+    public double get_speed() {
+        return _speed;
+    }
+
+    public void set_speed(double _speed) {
+        this._speed = _speed;
+    }
+
+    public Direction get_d() {
+        return _d;
+    }
+
+    public void set_d(Direction _d) {
+        this._d = _d;
     }
 
     public void deplacer() {
         switch (_d) {
-            case haut: _y--;
+            case haut: _y -= _speed;
             break;
 
-            case bas: _y++;
+            case bas: _y += _speed;
             break;
 
-            case gauche: _x--;
+            case gauche: _x -= _speed;
             break;
 
-            case droite: _x++;
+            case droite: _x += _speed;
             break;
 
             default: break;
