1533f2ccd449ba6f91c3dc85f93c5f8f65b2ebd2
Thiti <t.delplanque92@gmail.com>
2019-03-05 15:50:50 +0100

    Création de l'interface GraphicalEntity et ajout de méthodes dans Entity

--- src/librairie/Entity.java
+++ src/librairie/Entity.java
@@ -1,59 +1,67 @@
 package librairie;
 
+import java.util.Observable;
 import librairie.test.GraphicalEntity;
 
-public class Entity {
+public class Entity extends Observable{
 
-	protected double _x;
-	protected double _y;
-	protected CollisionBox _hitBox;
+    protected double _x;
+    protected double _y;
+    protected CollisionBox _hitBox;
+    protected GraphicalEntity _graphic;
 
-	public double getX() {
-		return _x;
-	}
+    public double getX() {
+        return _x;
+    }
 
-	public double getY() {
-            return _y;
-	}
-	
+    public double getY() {
+        return _y;
+    }
 
-	public void setX(double x) {
-            _x = x;
-	}
-	
 
-	public void setY(double y) {
-            _y = y;
-	}
-	
-	public Entity(double x, double y, CollisionBox hitBox) {
-		_x = x;
-		_y = y;
-		_hitBox = hitBox;
-	}
-	
-	public Entity(double x, double y, double height, double width) {
-		this(x, y, new CollisionBox(height, width));
-	}
-	
+    public void setX(double x) {
+        _x = x;
+    }
 
-	public void translate(double x, double y) {
-		_x = x;
-		_y = y;
-	}
-	
 
-	public boolean checkCollision(Entity obj) {
-		/**
-		 * vérification de la position :
-		 * trop à droite
-		 * trop à gauche
-		 * trop en bas
-		 * trop en haut
-		 */
-		return !((_x >= obj._x + obj._hitBox.getWidth())
-				|| (_x + _hitBox.getWidth() <= obj._x)
-				|| (_y >= obj._y + obj._hitBox.getHeight())
-				|| (_y + _hitBox.getHeight() <= obj._y));
-	}
+    public void setY(double y) {
+        _y = y;
+    }
+
+    public Entity(double x, double y, CollisionBox hitBox) {
+        _x = x;
+        _y = y;
+        _hitBox = hitBox;
+    }
+
+    public Entity(double x, double y, double height, double width) {
+        this(x, y, new CollisionBox(height, width));
+    }
+
+
+    public void translate(double x, double y) {
+        _x = x;
+        _y = y;
+    }
+
+
+    public boolean checkCollision(Entity obj) {
+        /**
+         * vérification de la position :
+         * trop à droite
+         * trop à gauche
+         * trop en bas
+         * trop en haut
+         */
+        return !((_x >= obj._x + obj._hitBox.getWidth())
+            || (_x + _hitBox.getWidth() <= obj._x)
+            || (_y >= obj._y + obj._hitBox.getHeight())
+            || (_y + _hitBox.getHeight() <= obj._y));
+    }
+    
+    public void setGraphicalEntity(GraphicalEntity entity){
+        if (entity == null)
+            throw new IllegalArgumentException();
+        _graphic = entity;
+    }
 }
