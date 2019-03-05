f7af92d90fd9433feeef8979b89c41cd06c22e61
Thiti <t.delplanque92@gmail.com>
2019-03-05 15:41:47 +0100

    réindentation de BoardManager

--- src/librairie/BoardManager.java
+++ src/librairie/BoardManager.java
@@ -5,41 +5,40 @@ import java.util.ArrayList;
 
 public class BoardManager {
 
-	private List<Entity> _entities = new ArrayList<Entity>();
-        private List<Entity> _movableEntities = new ArrayList<MovableEntity>();
+    private List<Entity> _entities = new ArrayList<Entity>();
+    private List<MovableEntity> _movableEntities = new ArrayList<MovableEntity>();
 
-	public void addEntity(Entity entity) {
-            _entities.add(entity);
-	}
+    public void addEntity(Entity entity) {
+       _entities.add(entity);
+    }
 
-	public boolean removeEntity(Entity entity) {
-            return _entities.remove(entity);
-	}
+    public boolean removeEntity(Entity entity) {
+        return _entities.remove(entity);
+    }
 
-        public void addMovableEntity(MovableEntity entity){
-            _movableEntities.add(entity);
-            if (!_entities.contains(entity)) {
-                addEntity(entity);
-            }
+    public void addMovableEntity(MovableEntity entity){
+        _movableEntities.add(entity);
+        if (!_entities.contains(entity)) {
+            addEntity(entity);
         }
+    }
 
-        public boolean removeMovableEntity(MovableEntity entity){
-            return _movableEntities.remove(entity) && _entities.remove(entity);
+    public boolean removeMovableEntity(MovableEntity entity){
+        return _movableEntities.remove(entity) && _entities.remove(entity);
+    }
+
+    public Entity checkCollision(Entity entity) {
+        for (Entity entityTested : _entities) {
+            if((entity != entityTested) && (entity.checkCollision(entityTested))) return entityTested; // sorry
         }
+        return null;
+    }
 
-	public entity checkCollision(Entity entity) {
-		for (Entity entityTested : _entities) {
-			if((entity != entityTested) && (entity.checkCollision(entityTested))) return true; // sorry
-		}
-		return null;
-	}
-
-        public void checkAllCollision(){
-            for(MovableEntity entityTested : _movableEntities){
-                Entity entity = checkCollision(entityTested);
-                entity != null ? entityTested.eventCollision(entity) :;
-            }
+    public void checkAllCollision(){
+        for(MovableEntity entityTested : _movableEntities){
+            Entity entity = checkCollision(entityTested);
+            if(entity != null)
+                entityTested.eventCollision(entity);
         }
-
-
+    }
 }
