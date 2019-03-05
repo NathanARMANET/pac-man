70bc4fefb41e3b810e3cf75883843b40d9ab3304
Thiti <t.delplanque92@gmail.com>
2019-03-05 15:26:35 +0100

    ajout d'une liste pour gérer les entitées qui bougent

--- src/librairie/BoardManager.java
+++ src/librairie/BoardManager.java
@@ -6,23 +6,42 @@ import java.util.ArrayList;
 public class BoardManager {
 	
 	private List<Entity> _entities = new ArrayList<Entity>();
+        private List<Entity> _movableEntities = new ArrayList<MovableEntity>();
 	
 	public void addEntity(Entity entity) {
-		_entities.add(entity);
+            _entities.add(entity);
 	}
 	
 	public boolean removeEntity(Entity entity) {
-		return _entities.remove(entity);
+            return _entities.remove(entity);
 	}
+        
+        public void addMovableEntity(MovableEntity entity){
+            _movableEntities.add(entity);
+            if (!_entities.contains(entity)) {
+                addEntity(entity);
+            }
+        }
+        
+        public boolean removeMovableEntity(MovableEntity entity){
+            return _movableEntities.remove(entity) && _entities.remove(entity);
+        }
 	
-	public boolean checkCollision(Entity entity) {
+	public entity checkCollision(Entity entity) {
 		for (Entity entityTested : _entities) {
 			if(entity != entityTested)
 				if(entity.checkCollision(entityTested))
-					return true; // sorry
+					return entityTested; // sorry
 		}
-		return false;
+		return null;
 	}
+        
+        public void checkAllCollision(){
+            for(MovableEntity entityTested : _movableEntities){
+                Entity entity = checkCollision(entityTested);
+                entity != null ? entityTested.eventCollision(entity) :;
+            }
+        }
 	
 
 }
