f549b7be588ca473075543ce1f6b36dc81a01f5e
Thiti <t.delplanque92@gmail.com>
2019-03-12 17:48:07 +0100

    ajout de la classe Wall

---
+++ src/jeu/entitées/Wall.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeu.entitées;
+
+import javafx.scene.Parent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import librairie.GraphicalEntity;
+import librairie.Entity;
+
+/**
+ *
+ * @author thiti
+ */
+public class Wall extends Parent{
+    
+    private Entity _entity;
+    private Shape _image;             
+
+    public Entity getEntity(){
+        return _entity;
+    }
+    
+    public Shape getImage(){
+        return _image;
+    }
+    
+    public Wall(double x, double y, double heigth, double width){
+        _entity = new Entity(x, y, heigth, width);
+        _image = new Rectangle(x,y,width,heigth);
+        _image.setFill(Color.BROWN);
+        this.getChildren().add(_image);
+    }
+}
